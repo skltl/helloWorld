4b57004074185a43e783a95b1f3e6bd2dc5ff200
skltl <as118410@gmail.com>
2018-08-04 12:26:07 +0200

    added final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String... args){
 
-        MessageCreator messageCreator = new MessageCreator();
+        final MessageCreator messageCreator = new MessageCreator();
 
         System.out.println(messageCreator.createMessage());
     }
